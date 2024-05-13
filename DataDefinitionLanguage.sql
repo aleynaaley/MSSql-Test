dfcd8b5cb100f6de5f310a0ea456780a1a9e2809
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-05-13 15:50:07 +0300

    Update DataDefinitionLanguage.sql

--- DataDefinitionLanguage.sql
+++ DataDefinitionLanguage.sql
@@ -11,5 +11,12 @@ Name_Student varchar(50),
 Surname_Student varchar(50),
 Birthdate_Student varchar(10)
 )
+GO
+
+CREATE TABLE Lesson(
+ ID_lesson INT PRIMARY KEY NOT NULL,
+ ID_Student INT FOREIGN KEY REFERENCES Student(ID_Student) NOT NULL,
+ Name_Lesson varchar(50),
+)
 
 --Student adlý tablo oluþturuldu ve ýd name surname ve býrthdate adlý kolonlar kuruldu
